d93f468a9caa5f1013830060d23b6ff1bb36be07
Adeel Akhtar <adeel.ahmed@10pearls.com>
2026-04-04 06:10:18 +0500

    Forcefully adding tfvars files

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -66,11 +66,11 @@ resource "aws_iam_role_policy_attachment" "ecr_read_only" {
 # ==============================
 # GITHUB ACTIONS OIDC ROLE
 # ==============================
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
-}
+#resource "aws_iam_openid_connect_provider" "github" {
+ # url             = "https://token.actions.githubusercontent.com"
+  #client_id_list  = ["sts.amazonaws.com"]
+  #thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+#}
 
 data "aws_iam_policy_document" "github_actions_assume_role" {
   statement {
