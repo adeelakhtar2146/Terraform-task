71456598bdc31cb207c3619a9fbb26e488111848
Adeel Akhtar <adeel.ahmed@10pearls.com>
2026-04-04 06:44:07 +0500

    Forcefully adding tfvars files

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -2,6 +2,11 @@ resource "aws_eks_cluster" "this" {
   name     = "${var.project_name}-${var.environment}-cluster"
   role_arn = var.cluster_role_arn
   vpc_config { subnet_ids = var.subnet_ids }
+
+  # YE HONA ZAROORI HAI Access Entry ke liye
+  access_config {
+    authentication_mode = "API_AND_CONFIG_MAP"
+  }
 }
 resource "aws_eks_node_group" "nodes" {
   cluster_name    = aws_eks_cluster.this.name
@@ -24,7 +29,10 @@ resource "aws_eks_access_entry" "adeel_user" {
 
 resource "aws_eks_access_policy_association" "adeel_admin" {
   cluster_name  = aws_eks_cluster.this.name
-  policy_arn    = "arn:aws:iam::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  
+  # YAHAN DEKHEIN: 'iam' ki jagah 'eks' hona chahiye
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  
   principal_arn = "arn:aws:iam::723626972500:user/eks-terraform"
 
   access_scope {
