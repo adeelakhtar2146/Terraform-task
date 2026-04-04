3faed4f3cf69c524109d94a00b429f3e5602d159
Adeel Akhtar <adeel.ahmed@10pearls.com>
2026-04-04 06:12:05 +0500

    Forcefully adding tfvars files

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -78,7 +78,7 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     actions = ["sts:AssumeRoleWithWebIdentity"]
     principals {
       type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.github.arn]
+      identifiers = ["arn:aws:iam::723626972500:oidc-provider/token.actions.githubusercontent.com"]
     }
     condition {
       test     = "StringEquals"
